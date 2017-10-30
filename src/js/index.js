8008dde1af94dc691a7bf3d5a9dfe88cab5bf0ca
Lover-fangjin <1063345449@qq.com>
2017-10-30 09:32:56 +0800

    jizhang(pc)

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import '../css/normalize.css';
+import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
+import 'Swiper/dist/css/swiper.css';
+import  Swiper from 'Swiper/dist/js/swiper.js';
+
+import '../css/index.css';
+import './picturefill.js';
